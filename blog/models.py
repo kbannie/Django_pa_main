07dd6fe66938596ca484ee79f9e2c1457c11eebe
unknown <kabin2205@gamil.com>
2022-05-04 14:26:55 +0900

    모델을 생성 + 시간 설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=50)  #짧은 내용
+    content=models.TextField()  #긴 내용
+
+    created_at=models.DateTimeField(auto_now_add=True) #현재시간 작성
+    updated_at = models.DateTimeField(auto_now=True) #수정된 시간만 수정
+    #author:추후 작성 예정
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
