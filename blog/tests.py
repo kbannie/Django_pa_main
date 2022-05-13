b68cde90b366f2985625c1b6f094b0da69756de4
unknown <kabin2205@gamil.com>
2022-05-13 19:31:34 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,9 +1,21 @@
 from django.test import TestCase, Client
+from django.contrib.auth.models import User
 from bs4 import BeautifulSoup
 from .models import Post
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump=User.objects.create_user(
+            username='trump',
+            password='somepassword',
+            author=self.user_trump
+        )
+
+        self.user_obama = User.objects.create_user(
+            username='obama',
+            password='somepassword',
+            author=self.user_obama
+        )
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -65,12 +77,15 @@ class TestView(TestCase):
 
         #3.4 "아직 게시물이 없습니다."라는 문구가 없어야한다.
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+        self.assertIn(post_001.author.username.upper(),main_area.text)
+        self.assertIn(post_002.author.username.upper(), main_area.text)
 
     def test_post_detail(self):
         # 1.1 포스트가 하나있다.
         post_001 = Post.objects.create(
             title='첫번째 포스트 입니다.',
             content='Hello,World. we are the World',
+            author=self.user_trump
         )
         self.assertEqual(Post.objects.count(), 1)
 
@@ -91,5 +106,6 @@ class TestView(TestCase):
         post_area = main_area.find('div', id='post-area')
         #self.assertIn(post_001.title, post_area.text)
         #2.5 첫번째 포스트의 작성자(author)가 포스트 영역에 있다.(아직 구현할 수 없음).
+        self.assertIn(self.user_trump.username.upper(),post_area.text)
         #2.6 첫번째 포스트의 내용(content)이 포스트 영역에 있다.
         #self.assertIn(post_001.content, post_area.text)
