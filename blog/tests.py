1bf22fb7728fc5159a9ae4ca5696a73389bef3fd
unknown <kabin2205@gamil.com>
2022-05-27 13:27:22 +0900

    포스트 상세 페이지에 카테고리 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -115,7 +115,6 @@ class TestView(TestCase):
         self.assertIn('아직 게시물이 없습니다.', main_area.text)
 
     def test_post_detail(self):
-
         self.assertEqual(Post.objects.count(), 3)
 
         #1.2 그 포스트의 url은 '/blog/1/'이다.
@@ -128,14 +127,15 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
         #2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
         self.navbar_test(soup)
+        self.category_card_test(soup)
 
-        #2.3 첫번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다.
+
         self.assertIn(self.post_001.title, soup.title.text)
-        #2.4 첫번째 포스트의 제목이 포스트 영역에 있다
+
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
         self.assertIn(self.post_001.title, post_area.text)
-        #2.5 첫번째 포스트의 작성자(author)가 포스트 영역에 있다.(아직 구현할 수 없음).
-        self.assertIn(self.user_trump.username.upper(),post_area.text)
-        #2.6 첫번째 포스트의 내용(content)이 포스트 영역에 있다.
+        self.assertIn(self.post_001.category.name, post_area.text)
+
+        self.assertIn(self.user_trump.username.upper(), post_area.text)
         self.assertIn(self.post_001.content, post_area.text)
